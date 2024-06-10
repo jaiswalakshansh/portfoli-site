dae02e42eb039c023e7e161982d730187173156f
Akshansh Jaiswal <49078770+jaiswalakshansh@users.noreply.github.com>
2024-06-10 18:37:48 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/App.jsx
+++ src/App.jsx
@@ -24,14 +24,14 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
+  name: "Akshansh Jaiswal",
+  title: "Security Researcher",
+  email: "akshansh@akshanshjaiswal.com",
+  gitHub: "jaiswalakshansh",
+  instagram: "akshansh_jaiswal",
+  linkedIn: "akshanshjaiswal",
   medium: "",
-  twitter: "microsoft",
+  twitter: "akshanshjaiswl",
   youTube: "Code",
 };
 
---
+++ src/Components/projectList.jsx
@@ -0,0 +1,28 @@
+/**
+ * Project list
+ *
+ * An array of objects that will be used to display for your project
+ * links section. Below is a sample, update to reflect links you'd like to highlight.
+ */
+export const projectList = [
+  {
+    title: "Hackerone mobile challenges walkthrough 🎉",
+    description: "I have recently completed the h101 ctf android challenges it was really fun learning and completing the challenges so here is the writeup ..",
+    url: "https://akshanshjaiswal.com/posts/h101/",
+  },
+  {
+    title: "Vulnerable learning app for security",
+    description: "Vuldroid is a Vulnerable Android Application made with security issues in order to demonstrate how they can occur in code.",
+    url: "https://github.com/jaiswalakshansh/Vuldroid",
+  },
+  {
+    title: "Collection of medium writeups",
+    description: "Collection of my techniocla writeups on medium.",
+    url: "https://akshanshjaiswal.medium.com/",
+  },
+  {
+    title: "Facebook security bug discovery",
+    description: "Story of a cool facebook bug found.",
+    url: "https://akshanshjaiswal.medium.com/pre-access-to-victims-account-via-facebook-signup-60219e9e381d",
+  },
+];
